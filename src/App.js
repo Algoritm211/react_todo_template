bc059a2a3c1974380fb14293eb82136326c4f52d
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-07-28 15:14:20 +0300

    react todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './Todo/AddTodo'
+// import onCreate from './Todo/AddTodo'
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Купить товар'},
+    {id: 2, completed: false, title: 'Купить товар 2'},
+    {id: 3, completed: false, title: 'Купить товар 3'},
+  ])
+  // let todos = [
+  //   {id: 1, completed: false, title: 'Купить товар'},
+  //   {id: 2, completed: false, title: 'Купить товар 2'},
+  //   {id: 3, completed: false, title: 'Купить товар 3'},
+  // ]
+
+  function toggleTodo(id) {
+    // console.log('todo id', id);
+    setTodos(todos.map((todo) => {
+      if (todo.id === id ) {
+        todo.completed = !todo.completed
+
+      }
+      return todo
+    })
+  )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+      title,
+      id: Date.now(),
+      completed: false,
+    }
+  ]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className='wrapper'>
+        <h1> React tutorial </h1>
+          <AddTodo onCreate={addTodo} />
+          {todos.length ? (
+
+          <TodoList todos={todos} onToggle = {toggleTodo}/>
+        ) : (
+          <p>No todos right now</p>
+        )
+          }
+      </div>
+    </Context.Provider>
   );
 }
 
